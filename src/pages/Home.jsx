2f52d643bbcecc5920c2b17b49971c86768f7857
BOOTVIRUS <kukuz_virus@virus.virus_d>
2025-08-13 18:22:13 +0300

    hero_modification

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import heroImage from '../assets/images/hero-image.jpg';
+import heroImage from '../assets/images/hero-image.png';
 
 const Home = () => {
   return (
